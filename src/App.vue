84ef615a559aa6572682c6d01dd338345c1f278c
Paul Pessoa <paulmspessoa@gmail.com>
2022-05-16 17:48:46 -0300

    finish info styles

--- src/App.vue
+++ src/App.vue
@@ -54,7 +54,7 @@ main {
   min-height: 100vh;
   padding: 25px;
 
-  background-image: linear-gradient(to botton, rgba(0, 0, 0, 0.25), rgba(0, 0, 0, 0.75));
+  background-image: linear-gradient(to bottom, rgba(0, 0, 0, 0.25), rgba(0, 0, 0, 0.75));
 }
 .search-box {
   widows: 100%;
@@ -85,6 +85,58 @@ main {
   background-color: rgba(255, 255, 255, 0.75);
   border-radius: 16px 0px 16px 0px;
 }
+
+.location-box .location {
+  color: #ffffff;
+  font-size: 32px;
+  font-weight: 500;
+  text-align: center;
+  text-shadow: 1px 3px rgba(0, 0, 0, 0.25);
+}
+
+.location-box .date {
+ color: #ffffff;
+  font-size: 20px;
+  font-weight: 300;
+  font-style: italic;
+  text-align: center;
+}
+
+.weather-box {
+  text-align: center;
+}
+
+.weather-box .temp {
+  display: inline-block;
+  padding: 10px 25px ;
+  color: #fff;
+  font-size: 102px;
+  font-weight: 900;
+
+  text-shadow: 3px 6px rgba(0, 0, 0, 0.75);
+  background-color: rgba(255, 255, 255, 0.25);
+  border-radius: 16px;
+  margin: 30px 0px; 
+
+  box-shadow: 3px 6px rgba(0, 0, 0, 0.75);
+}
+
+
+.weather-box .weather {
+  color: #fff;
+  font-size: 48px;
+  font-weight: 700;
+  font-style: italic;
+  text-shadow: 3px 6px rgba(0, 0, 0, 0.25);
+}
+
+
+
+
+
+
+
+
 
 </style>
 
