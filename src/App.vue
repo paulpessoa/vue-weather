693f5e5ed5df090c8a4d5dfa00aaa5091a648029
Paul Pessoa <paulmspessoa@gmail.com>
2022-05-16 17:03:58 -0300

    start search bar and style background

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,40 @@
 <template>
     <div id="app">
-
+      <main>
+        <div class="search-box">
+        <input type="text" class="search-bar" placeholder="Search ..."/>
+        </div>
+      </main>
     </div>
 </template>
 
 <script>
+// https://home.openweathermap.org/api_keys
 
 export default {
-    name: 'app'
+    name: 'app',
+    data () {
+      return {
+        api_key: 'b720c2d8764f53ecf466dcff89cc82de'
+      }
+    }
+
 }
 </script>
 
 <style>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
 
+body {
+  font-family: 'montserrat', sans-serif;
+}
+
+#app {
+  background-image: url('./assets/cold-bg.jpg');
+}
 </style>
+
