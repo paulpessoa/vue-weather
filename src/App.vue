64c6ef62e04452e3d97ebec9ac33deb3e7877abc
Paul Pessoa <paulmspessoa@gmail.com>
2022-05-16 18:23:40 -0300

    import data from API

--- src/App.vue
+++ src/App.vue
@@ -14,11 +14,11 @@
         <div class="weather-wrap" v-if="typeof weather.main !='undefined'">
           <div class="location-box">
             <div class="location">{{weather.name}}, {{weather.sys.country}}</div>
-            <div class="date">Segunda-Feira, 6 de maio de 2022</div>
+            <div class="date">{{ dateBuilder()}}</div>
           </div>
           <div class="weather-box">
-            <div class="temp">29C</div>
-            <div class="weather">Rain</div>
+            <div class="temp">{{Math.round(weather.main.temp) }}°C</div>
+            <div class="weather">{{weather.weather[0].main}}</div>
           </div>
         </div>
       </main>
@@ -49,6 +49,18 @@ export default {
       },
       setResults(results) {
         this.weather = results;
+      },
+      dateBuilder () {
+        let d = new Date();
+        let months = ["Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho", "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro"];
+        let days = ["Domingo", "Segunda-feira", "Terça-feira", "Quarta-feira", "Quinta-feira", "Sexta-feira", "Sábado"];
+
+        let day = days[d.getDay()];
+        let date = d.getDate();
+        let month = months[d.getMonth()];
+        let year = d.getFullYear();
+
+        return `${day} ${date} de ${month} de ${year}`;
       }
 
 
